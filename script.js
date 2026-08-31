b7638d4e07bff964e4df4e56a0ebf18e5a878b72
Arthur Hirata <arthirata2007@gmail.com>
2026-08-31 20:18:10 -0300

    Add api rules

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function login(){
     fetch(" http://127.0.0.1:8000/login", {
         method : 'GET'
     })
-    .then(data => Response.json)
-    alert(data)
+    .then(response => response.json())
+    .then(data =>{
+        alert(data.mensagem)
+    })
 }
