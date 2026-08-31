ef81f7cc28437606466549b6f096747bcfebc9d2
Arthur Hirata <arthirata2007@gmail.com>
2026-08-31 20:46:18 -0300

    Add the rate limit by IP

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime, timedelta
 app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",    
@@ -11,6 +13,33 @@ app.add_middleware(
     allow_methods=["*"],              
     allow_headers=["*"],             
 )
+ips_historic ={}
+req_limit = 10
+time_skip = 60
+@app.middleware("http")
+async def rate_limit_ip(request : Request, call_next):
+    client_ip = request.client.host
+    now = datetime.now()
+
+    if client_ip not in ips_historic:
+        ips_historic[client_ip] = []
+
+    limit_time = now - timedelta(seconds=time_skip)
+    ips_historic[client_ip] = [t for t in ips_historic[client_ip] if t > limit_time]
+
+    if len(ips_historic[client_ip]) >= req_limit:
+        return JSONResponse(
+            status_code=429,
+            content={
+                "succeso" : False,
+                "mensagem" : "Rate Limit excedido"
+            }
+        )
+    ips_historic[client_ip].append(now)
+    
+    response = await call_next(request)
+    return response
+
 @app.get("/login")
 def LoginUser():
     return {"mensagem" : "teste"}
--- script.js
+++ script.js
@@ -2,8 +2,20 @@ function login(){
     fetch(" http://127.0.0.1:8000/login", {
         method : 'GET'
     })
-    .then(response => response.json())
-    .then(data =>{
-        alert(data.mensagem)
+    .then( async response => {
+        if (response.status === 429){
+            alert("rate limit API")
+            return response.json()
+        }
+        if (response.ok){
+            return response.json()
+        }
+        if (!response){
+            throw new Error("erro servidor");
+        }
+        const data = await response.json()
+        console.log(data.mensagem)
     })
+    
+    
 }
