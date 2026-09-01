c14a244f94a0321e742b00b245d0b2a5256e1fea
Arthur Hirata <arthirata2007@gmail.com>
2026-09-01 14:29:50 -0300

    Add a rate limit on a secondary route

--- api.py
+++ api.py
@@ -14,11 +14,19 @@ app.add_middleware(
     allow_headers=["*"],             
 )
 ips_historic ={}
-req_limit = 10
-time_skip = 60
 @app.middleware("http")
 async def rate_limit_ip(request : Request, call_next):
     client_ip = request.client.host
+    route = request.scope.get("route")
+    time_skip = 60
+    req_limit = 10
+
+    for route in app.routes:
+        if hasattr(route, 'path') and route.path == request.url.path:
+            extra_dic = getattr(route, "openapi_extra", {})
+            req_limit = extra_dic.get("req_limit", 10)
+            time_skip = extra_dic.get("time_skip", 60)
+
     now = datetime.now()
 
     if client_ip not in ips_historic:
@@ -45,6 +53,14 @@ async def rate_limit_ip(request : Request, call_next):
     response = await call_next(request)
     return response
 
-@app.get("/login")
+@app.get("/login", openapi_extra={"req_limit" : 5, "time_skip" : 60})
 def LoginUser():
     return {"mensagem" : "teste"}
+
+
+num = 0
+@app.get("/increaseCount", openapi_extra={"req_limit" : 60, "time_skip": 120})
+def increaseCount():
+    global num
+    num += 1
+    return {"num" : num}
--- script.js
+++ script.js
@@ -14,10 +14,28 @@ function login() {
         }
 
         const data = await response.json();
-        console.log(data);
     })
     .catch(error => {
         console.error(error);
         alert("Erro de conexão ou CORS");
     });
+}
+
+function secondRoute(){
+    fetch("http://127.0.0.1:8000/increaseCount", {
+        method : 'GET'
+    })
+    .then(async response =>{
+        if (response.status === 429){
+            const data = await response.json().catch(() => ({}));
+            window.location.href = "429.html"
+            return;
+        }
+        if (!response.ok) {
+            throw new Error("erro servidor");
+        }
+        const data = await response.json();
+        const contagem = document.getElementById("contagem")
+        contagem.textContent = data.num
+    })
 }
